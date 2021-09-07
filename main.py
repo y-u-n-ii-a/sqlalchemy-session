a7fcc07727f52a51dfacdb309fe2b6f603adb113
Yuniia <yuniiaolkhova@gmail.com>
2021-09-07 22:59:55 +0300

    Add db

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from config import SQLALCHEMY_DATABASE_URI
-
+from model import meta, User
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
 if __name__ == '__main__':
+    meta.create_all(engine)
+
     session = Session(engine, autoflush=False, autocommit=False)
+    session.begin()
     print()
+    user = User()
+    user.name = 'yuniia'
+    session.flush()
     session.commit()
-    print('hello')
